50490fc5165353e66d5eb64b67d06f2a0eb9597c
Harkirpa <kirpa704@gmail.com>
2023-07-17 18:59:54 +0530

    handson2

---
+++ src/Component/Formclass.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import '../index.css'
+class Formclass extends Component {
+    state={
+        Name:'',
+        Dept:'',
+        Rate:'',
+        EmpData : []
+    }
+
+changeHandle=(event)=>{
+    this.setState({[event.target.name]:event.target.value})
+    // console.log(this.state.Name);
+}
+clickHandle=(e)=>{
+    e.preventDefault();
+    const newObj={
+        name:this.state.Name,
+        dept:this.state.Dept,
+         rate:this.state.Rate
+    }
+    this.state.EmpData.push(newObj)
+    this.setState({EmpData:this.state.EmpData})
+    
+       
+}
+    render(){
+         return (
+          <>
+          <h1 className="head">EMPLOYEE FEEDBACK FORM</h1>
+          <form>
+          <label htmlFor="name">Name :</label>
+          <input id="name" type="text" placeholder="Enter Name" name="Name" value={this.state.Name} onChange={this.changeHandle} required/>
+          <br/>
+          <br/>
+          <label htmlFor="dept">Department :</label>
+          <input id="dept" type="text" placeholder="Enter Department" name="Dept" value={this.state.Dept} onChange={this.changeHandle} required/>
+          <br/>
+          <br/>
+          <label htmlFor="rate">Rating :</label>
+          <input id="rate" type="text" placeholder="Enter rating" name="Rate" value={this.state.Rate} onChange={this.changeHandle} required/>
+          <br/>
+          <br/>
+          <br/>
+          <button id="submit" onClick={this.clickHandle}>Submit</button>
+          </form>
+         {
+          this.state.EmpData.map((item,index)=>{
+            return (
+                <>
+                <div id="box"></div>
+                <span>Name:{item.name} | Department:{item.dept} | Rating:{item.rate}</span>
+                
+                </>
+          )
+        })}
+            </>
+         )
+    }
+}
+     
+
+export default Formclass;
